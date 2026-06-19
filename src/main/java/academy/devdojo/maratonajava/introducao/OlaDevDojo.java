21412ac9499caf0ac4cffb4660fd323d0f659c26
Matheus Kenji <matheuskenjidev@gmail.com>
2026-06-19 13:43:17 -0300

    usando e entendendo comentários

--- src/main/java/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,17 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class OlaDevDojo {
+    //comentário de uma linha
+   /*
+   comentário de múltiplas linhas
+    */
+
+    /**
+     *
+     * isso é um comentário javadoc <p>teste</p>
+     *
+     * */
+
     public static void main(String[] args) {
         System.out.println("olá devdojo");
     }
