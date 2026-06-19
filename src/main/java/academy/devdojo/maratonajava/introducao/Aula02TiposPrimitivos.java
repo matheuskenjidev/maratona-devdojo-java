9d3f69993dfcf0d73fa74a53b57ad7bf3ef61b8e
Matheus Kenji <matheuskenjidev@gmail.com>
2026-06-19 14:07:59 -0300

    aprendendo casting e quando usar

--- src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,7 +3,7 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         //int, double, float, char, byte, short, long, boolean
-        int age = 23;
+        int age = (int) 2300000000L;
         System.out.println(age);
         double salary = 2345.77;
         float salarytwo = 2332.54F;
@@ -13,5 +13,6 @@ public class Aula02TiposPrimitivos {
         long superSalario = 230000000000000L;
         char sexo = 87; //tabela asc2/unicode
         System.out.println(sexo);
+        System.out.println(age);
     }
 }
