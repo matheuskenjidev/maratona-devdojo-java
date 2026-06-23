9195577199164eac5f20df1baef31942e0df59a0
Matheus Kenji <matheuskenjidev@gmail.com>
2026-06-22 22:00:01 -0300

    operadores Aritméticos em java

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula03String.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03String {
+    public static void main(String[] args) {
+        String nome = "Matheus";
+        System.out.println(nome);
+
+    }
+}
