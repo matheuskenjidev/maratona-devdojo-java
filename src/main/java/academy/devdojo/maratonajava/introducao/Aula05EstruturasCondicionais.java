8a7e4a95960d012903a6f7f6fad15bd6cf90957a
Matheus Kenji <matheuskenjidev@gmail.com>
2026-07-14 20:41:50 -0300

    aprendendo sobre a estrutura condicional if

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 15;
+
+        if (idade >= 18) {
+            System.out.println("Dentro do if");
+        } else {
+            System.out.println("dentro do else!");
+        }
+    }
+}
