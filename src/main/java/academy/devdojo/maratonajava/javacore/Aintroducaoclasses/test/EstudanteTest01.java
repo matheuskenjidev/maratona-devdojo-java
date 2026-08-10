308f54c723b3ef842270be99b7698e51a6a21ba4
Matheus Kenji <matheuskenjidev@gmail.com>
2026-08-10 16:38:48 -0300

    estudando sobre classes

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+        estudante.nome = "Matheus";
+        estudante.idade = 23;
+        estudante.sexo = 'M';
+        System.out.println(estudante);
+    }
+}
