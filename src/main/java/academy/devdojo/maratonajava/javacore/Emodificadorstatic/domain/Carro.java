30c28275873ed3a471a256352e8b7fb599dd5a00
Matheus Kenji <matheuskenjidev@gmail.com>
2026-08-12 21:44:50 -0300

    atributos static

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Emodificadorstatic/domain/Carro.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.Emodificadorstatic.domain;
+
+public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    public static double velocidadeLimite = 250;
+
+    public void imprime() {
+        System.out.println("------------------");
+        System.out.println("Nome " + this.nome);
+        System.out.println("Velocidade máxima " + this.velocidadeMaxima);
+        System.out.println("Velocidade Limite " + Carro.velocidadeLimite);
+    }
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Emodificadorstatic/test/CarroTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Emodificadorstatic.test;
+
+import academy.devdojo.maratonajava.javacore.Emodificadorstatic.domain.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedez", 275);
+        Carro c3 = new Carro("Porsche", 290);
+
+        Carro.velocidadeLimite = 46;
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+    }
+}
