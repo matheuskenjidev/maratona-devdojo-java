d999d9417a17173210cf72f46a89f9a2fe59b42a
Matheus Kenji <matheuskenjidev@gmail.com>
2026-08-14 22:57:02 -0300

    bloco de inicializacao static

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Emodificadorstatic/domain/Anime.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava.javacore.Emodificadorstatic.domain;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+
+    static {
+        System.out.println("Dentro do bloco de inicializacao normal");
+        episodios = new int[100];
+        for(int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(int[] episodios) {
+        this.episodios = episodios;
+    }
+}
---
+++ src/main/java/academy/devdojo/maratonajava/javacore/Emodificadorstatic/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Emodificadorstatic.test;
+
+import academy.devdojo.maratonajava.javacore.Emodificadorstatic.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime("Hxh");
+        Anime anime2 = new Anime("Monster HUNTER");
+    }
+}
